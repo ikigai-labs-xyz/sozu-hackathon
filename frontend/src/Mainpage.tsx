b5bce8de3ebf248e3b2b4adbf24a2ba43392b52e
Valerio <78494380+valeriofichera@users.noreply.github.com>
2023-07-16 20:40:57 +0200

    added mainpage + tailwind

---
+++ frontend/src/Mainpage.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.tsx';
+import MainBox from './components/MainBox.tsx';
+
+function MainPage() {
+
+  return (
+
+<>
+<Header/>
+<MainBox/>
+</>
+
+  );
+}
+
+export default MainPage;
