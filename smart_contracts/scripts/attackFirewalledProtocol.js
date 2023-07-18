20af3f032f1091e9f47937bbe79efa7a125df9e9
keinberger <philipp.keinberger@gmail.com>
2023-07-19 01:56:56 +0200

    implement firewalled attack

--- smart_contracts/scripts/attackFirewalledProtocol.js
+++ smart_contracts/scripts/attackFirewalledProtocol.js
@@ -7,24 +7,24 @@ const main = async hre => {
   const attackContractAddress = await attackContract.getAddress()
   console.log(`Attack contract address: ${attackContractAddress}`)
 
-  const nonFirewalledProtocol = await ethers.getContract("NonFirewalledProtocol")
-  const nonFirewalledProtocolAddress = await nonFirewalledProtocol.getAddress()
-  console.log(`Non firewalled protocol address: ${nonFirewalledProtocolAddress}`)
+  const firewalledProtocol = await ethers.getContract("FirewalledProtocol")
+  const firewalledProtocolAddress = await firewalledProtocol.getAddress()
+  console.log(`Non firewalled protocol address: ${firewalledProtocolAddress}`)
 
   // usdc balance before hack of protocol
-  const usdcBalanceBeforeProtocol = await usdc.balanceOf(nonFirewalledProtocolAddress)
+  const usdcBalanceBeforeProtocol = await usdc.balanceOf(firewalledProtocolAddress)
   console.log(`USDC balance of non firewalled protocol before hack: ${usdcBalanceBeforeProtocol.toString()}`)
 
   // usdc balance of attacker before hack
   const usdcBalanceBeforeAttacker = await usdc.balanceOf(attackContractAddress)
   console.log(`USDC balance of attacker before hack: ${usdcBalanceBeforeAttacker.toString()}`)
 
-  const attackTx = await attackContract.attack(nonFirewalledProtocolAddress)
+  const attackTx = await attackContract.attack(firewalledProtocolAddress, { gasLimit: 1000000 })
   await attackTx.wait()
 
   console.log(`Attack complete`)
   // usdc balance of non firewalled protocol
-  const usdcBalanceProtocol = await usdc.balanceOf(nonFirewalledProtocolAddress)
+  const usdcBalanceProtocol = await usdc.balanceOf(firewalledProtocolAddress)
   console.log(`USDC balance of non firewalled protocol: ${usdcBalanceProtocol.toString()}`)
   // usdc balance of attacker
   const usdcBalanceAttacker = await usdc.balanceOf(attackContractAddress)
